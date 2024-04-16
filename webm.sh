5214354c470b3d971d3e06ebb68b45c754d857ae
Jacky Zhao <j.zhao2k19@gmail.com>
2024-04-16 15:00:17 -0700

    change codec

--- webm.sh
+++ webm.sh
@@ -6,7 +6,7 @@ convert_to_webm() {
   echo "converting ${input_file}"
   local output_file="${input_file%.*}.webm"
   
-  ffmpeg -i "$input_file" -c:v libvpx-vp9 -crf 30 -b:v 0 -b:a 128k -c:a libopus "$output_file"
+  ffmpeg -i "$input_file" -f webm -c:v libvpx -b:v 2M -an -auto-alt-ref 0 "$output_file" -hide_banner -nostdin
 }
 
 # Recursively find and convert MP4 files
