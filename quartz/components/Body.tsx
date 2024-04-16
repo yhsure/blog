741ca30d71cb1d1984d983ae95e9b528110f449b
Jacky Zhao <j.zhao2k19@gmail.com>
2024-04-16 00:13:04 -0700

    lazy load all the things

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,5 +1,7 @@
 // @ts-ignore
 import clipboardScript from "./scripts/clipboard.inline"
+// @ts-ignore
+import lazyLoadScript from "./scripts/lazy.inline"
 import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
@@ -7,7 +9,7 @@ const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
   return <div id="quartz-body">{children}</div>
 }
 
-Body.afterDOMLoaded = clipboardScript
+Body.afterDOMLoaded = clipboardScript + lazyLoadScript
 Body.css = clipboardStyle
 
 export default (() => Body) satisfies QuartzComponentConstructor
