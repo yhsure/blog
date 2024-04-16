d6aa3ed8f434b2afee3e6fe10fbef3056f7599fe
Jacky Zhao <j.zhao2k19@gmail.com>
2024-04-16 09:12:39 -0700

    convert .mov -> .webm

---
+++ webm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Function to convert video to WebM
+convert_to_webm() {
+  local input_file="$1"
+  echo "converting ${input_file}"
+  local output_file="${input_file%.*}.webm"
+  
+  # ffmpeg -i "$input_file" -c:v libvpx-vp9 -crf 30 -b:v 0 -b:a 128k -c:a libopus "$output_file"
+  ffmpeg -i "$input_file" -c:v libsvtav1 -preset 4 -crf 30 -g 240 -pix_fmt yuv420p10le -svtav1-params tune=0:film-grain=8 -b:a 128k -nostdin -c:a libopus "$output_file" 
+}
+
+# Recursively find and convert MP4 files
+find ./content -type f -name "*.mov" -print0 | while IFS= read -r -d '' file; do
+  convert_to_webm "$file"
+done
