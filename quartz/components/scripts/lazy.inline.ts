29a8490fec13b8f79a1054b72d4377c38d65f723
Jacky Zhao <j.zhao2k19@gmail.com>
2024-08-03 13:20:00 -0700

    tailscale notes

--- quartz/components/scripts/lazy.inline.ts
+++ quartz/components/scripts/lazy.inline.ts
@@ -1,10 +1,9 @@
-import lozad from "lozad";
+import lozad from "lozad"
 
 document.addEventListener("nav", (evt) => {
   const url = evt.detail.url
   if (url === "thoughts/craft") {
-    const observer = lozad('.lazy');
-    observer.observe();
+    const observer = lozad(".lazy")
+    observer.observe()
   }
 })
-
