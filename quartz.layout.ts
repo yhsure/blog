0e67c9fa575fde3a82ed6cfe66ea815029d8edbe
Jacky Zhao <j.zhao2k19@gmail.com>
2025-03-23 20:51:40 -0700

    remove alias redirect and breadcrumbs

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,10 +49,6 @@ const left = [
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -74,7 +70,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.ArticleTitle(), Component.ContentMeta()],
   left,
   right: [],
 }
