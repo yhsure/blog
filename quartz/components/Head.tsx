53af8e5bfb434932ca84343be36fa8f2d7366b22
yhsure <andbje@hotmail.com>
2025-07-11 20:22:01 +0200

    attempt iOS margin fix

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -52,7 +52,7 @@ export default (() => {
           </>
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
 
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
