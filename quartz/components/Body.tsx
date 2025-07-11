512e75ce6a64f833b93f7f9c8d5ac2e524f0d925
yhsure <andbje@hotmail.com>
2025-07-11 20:09:18 +0200

    fix pesky sidebar track using custom sidebar

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -2,6 +2,8 @@
 import clipboardScript from "./scripts/clipboard.inline"
 // @ts-ignore
 import lazyLoadScript from "./scripts/lazy.inline"
+// @ts-ignore
+import customScrollbarScript from "./scripts/custom-scrollbar.inline"
 import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
@@ -9,7 +11,7 @@ const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
   return <div id="quartz-body">{children}</div>
 }
 
-Body.afterDOMLoaded = clipboardScript + lazyLoadScript
+Body.afterDOMLoaded = clipboardScript + lazyLoadScript + customScrollbarScript
 Body.css = clipboardStyle
 
 export default (() => Body) satisfies QuartzComponentConstructor
